2ca09697d760404748d55d25b381807a813a0425
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-01-22 21:59:27 +0530

    Add neccessary files

---
+++ src/Components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import About from './About'
+import Skills from './Skills'
+import Projects from './Projects'
+import Qualification from './Qualification'
+
+function Main() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Skills/>
+    <Projects/>
+    <Qualification/>
+    </>
+  )
+}
+
+export default Main
---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <div className='Navbar'>
+    <div>{'<Shreyansh/>'}</div>
+    <div>
+        <a>home</a>
+        <a>about</a>
+        <a>Skills</a>
+        <a>Portfolio</a>
+        <a>contact</a>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
