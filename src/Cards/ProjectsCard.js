b81c02b4e40217c3853f4b3ec9ff798addea409f
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-01-25 20:45:32 +0530

    Modified avatar image

---
+++ src/Cards/ProjectsCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProjectsCard() {
+  return (
+    <div>ProjectsCard</div>
+  )
+}
+
+export default ProjectsCard
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProjectsCard from '../Cards/ProjectsCard'
 
 function Projects() {
+  
   return (
-    <div>Projects</div>
+    <>
+    <div className='conatiner-projects'>
+        <h1 className='about-page-heading'>Projects</h1>
+        <p className='about-page-subheading'>My Recent Works</p>
+      <div className='main-projects'>
+        <ProjectsCard/>
+      </div>
+    </div>
+    </>
   )
 }
 
