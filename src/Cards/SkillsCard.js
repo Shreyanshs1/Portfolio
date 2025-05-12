b87f6e9504a3e353a7d74a5aedfb6c2a9b061476
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-05-12 15:32:55 +0530

    Add framer motion to skills section

--- src/Cards/SkillsCard.js
+++ src/Cards/SkillsCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './SkillsCard.css'
 import verified from '../assets/Logos/verified.svg'
+import { motion } from 'framer-motion';
 
-function SkillsCard({ heading, list }) {
+function SkillsCard({ heading, list,slideDirection }) {
   const items = [];
+  const initialX = slideDirection === 'left' ? -100 : 100;
   list.forEach((item, index) => {
     items.push(
       <div className='skills-list-card'>
@@ -14,12 +16,17 @@ function SkillsCard({ heading, list }) {
 
   return (
     <>
-    <div className='skillscard-box'>
+    <motion.div
+      initial={{ opacity: 0, x: initialX }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, ease: 'easeOut' }}
+      viewport={{ once: true, amount: 0.2 }}
+     className='skillscard-box'>
       <h2 className='skills-heading'>{heading}</h2>
       <ul className='ul-skills'>
         {items}
       </ul>
-    </div>
+    </motion.div>
     </>
   );
 }
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,10 +4,10 @@ import SkillsCard from '../Cards/SkillsCard'
 import line from '../assets/Line-Design.svg'
 
 function Skills() {
-  let frontendList=["HTML","CSS","React"];
-  let backendList =["Nodejs","Express.js","Restful API"]
-  let languages = ["C++","C","Java","JavaScript"];
-  let databases = ["MongoDB","MySQL"];
+  let frontendList=["HTML","CSS","Reactjs"];
+  let backendList =["Nodejs","Express.js","Restful API","ASP.NET Web API"];
+  let languages = ["C/C++","C#","Java","JavaScript"];
+  let databases = ["MongoDB","MySQL","SQL Server"];
 
   return (
     <>
@@ -15,10 +15,10 @@ function Skills() {
     <h1 className='about-page-heading'>Skills</h1>
     <p className='about-page-subheading'>My Skills</p>
       <div className='main-skills'>
-      <SkillsCard heading="Frontend" list={frontendList}/>
-      <SkillsCard heading="Backend" list={backendList}/>
-      <SkillsCard heading="Languages" list={languages}/>
-      <SkillsCard heading="Databases" list={databases}/>
+      <SkillsCard heading="Frontend" list={frontendList} slideDirection="left"/>
+      <SkillsCard heading="Backend" list={backendList} slideDirection="right"/>
+      <SkillsCard heading="Languages" list={languages} slideDirection="left"/>
+      <SkillsCard heading="Databases" list={databases} slideDirection="right"/>
       </div>
       <div className='line-design'>
         <img src={line}/>
