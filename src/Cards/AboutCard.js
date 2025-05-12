14b78fb7eb8a06f9fbddd091a21f9d5902ea992f
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-05-12 15:15:00 +0530

    Add framer motion to the about section

--- src/Cards/AboutCard.js
+++ src/Cards/AboutCard.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './AboutCard.css'
+import { motion } from 'framer-motion';
 
-function AboutCard({logo,heading,details}) {
+function AboutCard({ logo, heading, details }) {
   return (
     <>
-    <div className='about-card'>
-        <img className='topic-logo' src={logo}/>
+      <motion.div
+        className='about-card'
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: 'easeOut' }}
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <img className='topic-logo' src={logo} />
         <p className='topic-heading'>{heading}</p>
         <p className='topic-details'>{details}</p>
-    </div>
+      </motion.div>
     </>
   )
 }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import avatar from '../assets/avatar.png'
+import avatar from '../assets/avatar2.png'
 import '../StyleSheets/About.css'
 import AboutCard from '../Cards/AboutCard'
 import Work from '../assets/Logos/work.svg'
@@ -8,16 +8,28 @@ import Github from '../assets/Logos/github.svg'
 import Resume from '../assets/resume.docx'
 import File from '../assets/Logos/file.svg'
 import line from '../assets/Line-Design.svg'
+import { motion } from 'framer-motion';
 
 function About() {
   return (
     <>
     <div className='container-about' id='about'>
-        <h1 className='about-page-heading'>About Me</h1>
+        <motion.h1 
+        className='about-page-heading'
+        initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.2, duration: 0.4 }}
+                viewport={{ once: true, amount: 0.2 }}
+        >About Me</motion.h1>
         <p className='about-page-subheading'>My Introduction</p>
         <div className='main-about'>
         <div className='left-about'>
-          <img src={avatar} className='avatar-img about-img'/>
+          <motion.img
+          initial={{ opacity: 0, scale: 0.8 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.4 }}
+                viewport={{ once: true, amount: 0.2 }}
+           src={avatar} className='avatar-img about-img'/>
         </div>
         <div className='right-about'>
           <div className='about-cards'>
@@ -26,12 +38,20 @@ function About() {
             <AboutCard logo={Github} heading="Github" details="9 Repos"/>
           </div>
           <div className='my-intro-about'>
-            <p className='my-intro-about-para'>Software Developer with knowledge of C++, Java and web development technologies (HTML, CSS, Javascript, React, Node.Js, Express.Js). Eager to learn and contribute to innovative projects.</p>
+            <motion.p
+            initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.2, duration: 0.4 }}
+                viewport={{ once: true, amount: 0.2 }}
+             className='my-intro-about-para'>Software Developer with knowledge of C++, Java and web development technologies (HTML, CSS, Javascript, React, Node.Js, Express.Js). Eager to learn and contribute to innovative projects.</motion.p>
           </div>
-            <a href={Resume} download className='about-resume-btn'>
+            <motion.a
+            whileHover={{ scale: 1.1 }}
+  whileTap={{ scale: 0.8 }}
+             href={Resume} download className='about-resume-btn'>
               Download CV
               <img src={File} className='dwnld-file-logo'/>
-            </a>
+            </motion.a>
         </div>
       </div>
       <div className='line-design'>
