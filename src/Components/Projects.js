0c575568c32766312974f0700656294aa3a3babb
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-09-18 11:07:27 +0530

    Add tabs to project section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectsCard from '../Cards/ProjectsCard'
 import Visualizer from '../assets/Screenshot/visualizer.jpeg'
 import Todo from '../assets/Screenshot/todo.jpeg'
@@ -14,6 +14,8 @@ import '../StyleSheets/Projects.css'
 import line from '../assets/Line-Design.svg'
 
 function Projects() {
+  const [activeTab, setActiveTab] = useState('all')
+
   const projects = [
     {
       id:0,
@@ -21,7 +23,8 @@ function Projects() {
       image:Discord,
       github:"https://github.com/Shreyanshs1/Discord",
       demo:null,
-      techStack: ["C#", "WPF", "XAML","Socket"]
+      techStack: ["C#", "WPF", "XAML","Socket"],
+      type: "Desktop"
     },
     {
       id:1,
@@ -29,7 +32,8 @@ function Projects() {
       image:Career,
       github:"https://github.com/Shreyanshs1/CareerArcade",
       demo:"https://careerarcade.vercel.app/",
-      techStack: ["React", "ASP.NET Web API", "SQL Server"]
+      techStack: ["React", "ASP.NET Web API", "SQL Server"],
+      type: "Web"
     },
     {
       id:2,
@@ -37,7 +41,8 @@ function Projects() {
       image:Expense,
       github:"https://github.com/Shreyanshs1/ExpenseTracker",
       demo:"https://expensetracker-eta-fawn.vercel.app/login",
-      techStack: ["React", "Node.js","Express", "MongoDB"]
+      techStack: ["React", "Node.js","Express", "MongoDB"],
+      type: "Web"
     },
     {
       id:3,
@@ -45,7 +50,8 @@ function Projects() {
       image:Visualizer,
       github:"https://github.com/Shreyanshs1/Sorting-Visualizer.git",
       demo:"https://shreyanshs1.github.io/Sorting-Visualizer/",
-      techStack: ["HTML", "CSS", "JavaScript"]
+      techStack: ["HTML", "CSS", "JavaScript"],
+      type: "Web"
     },
     {
       id:4,
@@ -53,7 +59,8 @@ function Projects() {
       image:Todo,
       github:"https://github.com/Shreyanshs1/todo.git",
       demo:null,
-      techStack: ["React", "Node.js","Express", "MongoDB"]
+      techStack: ["React", "Node.js","Express", "MongoDB"],
+      type: "Web"
     },
     {
       id:5,
@@ -61,7 +68,8 @@ function Projects() {
       image:Dictionary,
       github:"https://github.com/Shreyanshs1/Dictionary",
       demo:"https://shreyanshs1.github.io/Dictionary/",
-      techStack: ["HTML", "CSS", "JavaScript"]
+      techStack: ["HTML", "CSS", "JavaScript"],
+      type: "Web"
     }
     ,{
       id:6,
@@ -69,7 +77,8 @@ function Projects() {
       image:Currency,
       github:"https://github.com/Shreyanshs1/Currency-Converter",
       demo:"https://shreyanshs1.github.io/Currency-Converter/",
-      techStack: ["HTML", "CSS", "JavaScript"]
+      techStack: ["HTML", "CSS", "JavaScript"],
+      type: "Web"
     },
     {
       id:7,
@@ -77,7 +86,8 @@ function Projects() {
       image:Weather,
       github:"https://github.com/Shreyanshs1/react-weather-app/tree/main",
       demo:null,
-      techStack: ["React","Weather API"]
+      techStack: ["React","Weather API"],
+      type: "Web"
     },
     {
       id:8,
@@ -85,7 +95,8 @@ function Projects() {
       image:Calculator,
       github:"https://replit.com/@Shreyansh1807/React-Calculator",
       demo:null,
-      techStack: ["React"]
+      techStack: ["React"],
+      type: "Web"
     },
     {
       id:9,
@@ -93,21 +104,46 @@ function Projects() {
       image:SoftSell,
       github:"https://github.com/Shreyanshs1/softsell",
       demo:"https://softsell-phi-three.vercel.app/",
-      techStack: ["HTML", "CSS", "JavaScript"]
+      techStack: ["HTML", "CSS", "JavaScript"],
+      type: "Web"
     }
   ]
+
+  const filteredProjects = activeTab === 'all'
+    ? projects
+    : projects.filter(project => project.type.toLowerCase() === activeTab)
+
   return (
     <>
     <div className='container-projects' id='projects'>
         <h1 className='about-page-heading'>Projects</h1>
         <p className='about-page-subheading'>My Recent Works</p>
+        <div className='projects-tabs'>
+          <button
+            className={`projects-tab-btn${activeTab === 'all' ? ' active' : ''}`}
+            onClick={() => setActiveTab('all')}
+          >
+            All
+          </button>
+          <button
+            className={`projects-tab-btn${activeTab === 'web' ? ' active' : ''}`}
+            onClick={() => setActiveTab('web')}
+          >
+            Web
+          </button>
+          <button
+            className={`projects-tab-btn${activeTab === 'desktop' ? ' active' : ''}`}
+            onClick={() => setActiveTab('desktop')}
+          >
+            Desktop
+          </button>
+        </div>
       <div className='main-projects'>
         {
-          projects.map((project)=>(
+          filteredProjects.map((project)=>(
             <ProjectsCard key={project.id} project={project}/>
           ))
         }
-        
       </div>
       <div className='line-design'>
         <img src={line}/>
